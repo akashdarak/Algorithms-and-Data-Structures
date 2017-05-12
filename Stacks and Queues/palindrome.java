3a131f7f45c8a484dc5a3650a7eb615f9fd77231
Akash Darak <akash.darak@colostate.edu>
2017-05-11 20:55:49 -0600

    Update palindrome.java

--- Stacks and Queues/palindrome.java
+++ Stacks and Queues/palindrome.java
@@ -1,1 +1,26 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+    
+    Stack<Character> stack = new Stack<>();
+    
+    Queue<Character> queue = new LinkedList<>();
+    
+    public void pushCharacter(char ch){
+        stack.push(ch);
+    }
+        
+    public void enqueueCharacter(char ch) {
+        queue.add(ch);
+    }
+    
+    public char popCharacter(){
+        return stack.pop();
+    } 
+    
+    public char dequeueCharacter(){
+        return (char) queue.remove();
+    }
+
+  
